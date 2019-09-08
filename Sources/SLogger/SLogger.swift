c60320e219dcc456479bb3c304434fcd42f7b460
Nikolay Fiantsev <to.bigmotor@gmail.com>
2019-09-08 22:13:36 +0300

    Make methods public

--- Sources/SLogger/SLogger.swift
+++ Sources/SLogger/SLogger.swift
@@ -1,22 +1,22 @@
-struct SLogger {
-  static func debug(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
+public struct SLogger {
+  public static func debug(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
     print("\(file), \(line)\n🛠 Debug: \(message())")
   }
   
-  static func info(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
+  public static func info(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
     print("\(file), \(line)\n✅ Info: \(message())")
   }
   
-  static func warning(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
+  public static func warning(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
     print("\(file), \(line)\n⚠️ Warning: \(message())")
   }
   
-  static func error(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
+  public static func error(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
     let messageTxt = "Error: \(message())"
     print("\(file), \(line)\n⛔️ \(messageTxt)")
   }
   
-  static func fatal(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
+  public static func fatal(_ message: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line) {
     print("\(file), \(line)\n❌ Fatal: \(message())")
     assertionFailure()
   }
